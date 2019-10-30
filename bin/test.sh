7e42eacd50ab0360f2053c09a939f116748164b9
tasuku-ito <tasukuito5926@gmail.com>
2019-10-30 13:42:13 +0900

    修正したよ

--- bin/test.sh
+++ bin/test.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 
 pip install --upgrade pip
-pip install -r ../src/requirements.txt
-pip install -r ../tests/requirements.txt
+pip install -r src/requirements.txt
+pip install -r tests/requirements.txt
 pytest
